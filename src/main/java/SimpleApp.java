800163c20a68b5428556c458de53da501c5f722b
Bráulio Figueiredo <braulio@braulioti.com.br>
2021-01-19 10:20:22 -0300

    Created a basic application

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,13 @@
+public class SimpleApp {
+    public static void main(String arg[]) throws Exception {
+
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("SIMPLE WEB APPLICATION FOR DESKTOP SOLUTIONS");
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("Author: Bráulio Figueiredo");
+        System.out.println("Twitter: _brau_io");
+        System.out.println("E-mail: braulio@braulioti.com.br");
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("");
+    }
+}
